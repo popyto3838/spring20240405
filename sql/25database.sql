c1fa34720f781f00297e45dc71fbcbf5c9781b50
Hongbin Yoon <popyto@naver.com>
2024-04-25 16:32:44 +0900

    commit the first

---
+++ sql/25database.sql
@@ -0,0 +1,36 @@
+# 사용할 데이터베이스(스키마) 선택하기
+USE w3schools;
+
+SHOW tables;
+
+# 새 데이터베이스 만들기
+
+CREATE DATABASE mydb1;
+USE mydb1;
+SHOW tables;
+CREATE TABLE my_table1
+(
+    name VARCHAR(3),
+    age  INT
+);
+
+# todo : mydb2 데이터베이스 만들고 옮겨서
+# 새테이블 하나 만들기
+CREATE DATABASE mydb2;
+USE mydb2;
+SHOW tables;
+CREATE TABLE my_table1
+(
+    name VARCHAR(10),
+    age  INT
+);
+SELECT*
+FROM my_table1;
+SELECT *
+FROM mydb1.my_table1;
+
+DROP DATABASE mydb2;
+
+#todo mydb1 삭제
+DROP DATABASE mydb1;
+
---
+++ sql/28thirdNormalForm.sql
@@ -0,0 +1,45 @@
+#ThirdNormal Form(3NF)
+
+# 1) 2NF 이다.
+# 2) 키가 아닌 컬럼끼리 종속되면 안됨
+
+
+USE mydb1;
+
+DROP TABLE customer;
+DROP TABLE customer_benefit;
+CREATE TABLE customer
+(
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(3),
+    last_name  VARCHAR(3),
+    level      INT,
+    FOREIGN KEY (level) REFERENCES customer_benefit (level)
+);
+CREATE TABLE customer_benefit
+(
+
+    level   INT PRIMARY KEY,
+    benefit VARCHAR(100)
+);
+
+
+
+INSERT INTO customer
+    (first_name, last_name, level)
+VALUES ('son', 'hm', 1),
+       ('lee', 'ki', 1),
+       ('kim', 'hs', 2),
+       ('lee', 'js', 2),
+       ('cap', 'ste', 3);
+INSERT INTO customer_benefit
+    (level, benefit)
+VALUES (1, '무료배송'),
+       (2, '할인'),
+       (3, '라운지');
+
+SELECT *
+FROM customer_benefit;
+
+SELECT *
+FROM customer;
